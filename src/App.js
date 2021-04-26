1afb5ff501e5ee7c01e19e5c317e4a8a174b70b2
David Delgadillo <daviddel.731@gmail.com>
2021-04-26 13:56:32 -0700

    Added creature data!

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { Component } from 'react';
-import logo from './react-logo.svg';
 import './App.css';
 import React from 'react';
 
@@ -10,9 +9,6 @@ class App extends Component {
       <div className="App">
   
         My React App...
-        
-        <img src={logo} className="temp-images" alt="react logo" />
-        <img src="acl-logo.png" className="temp-images" alt="acl logo" />
       
       </div>
     );
